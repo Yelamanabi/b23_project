2d61198ae8ff00e9167fdc999559cf052f7602af
Yelamanabi <Yelamanabi@gmail.com>
2021-07-26 02:05:34 -0500

    added git account

--- src/GitCool.java
+++ src/GitCool.java
@@ -3,4 +3,9 @@ public class GitCool {
     // b23 is a special number
     //b23 is best
 
+    //we were able to do succefully shared project under our github account
+    //share project on git hub happen only one time per prject
+    //it comboimes the 3 actions below
+    // create remotes responsistort under your account
+
 }
